c285a1252a284a2d994930f3f6b5d110829486ed
Julio Sandino <sandinojulio98@gmail.com>
2023-09-23 23:04:42 -0700

    "cleaning up main.c"

--- main/main.c
+++ main/main.c
@@ -85,9 +85,6 @@ void sensor_task(void *pvParamaters) {
     }
 }
 
-/*
-* 
-*/
 void display_sensor_data_task(void *pvParameters) {
     /*
     /This task displays sensor data to the OLED display
@@ -143,203 +140,9 @@ void display_sensor_data_task(void *pvParameters) {
             free(humidity_string);
             free(moisture_string);
             
-            
         }
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
-}
-
-
-int read_temp_sensor(float* temperature, float* humidity) {
-    /*
-    Reads temperature and humidity from a DHT11 sensor
-
-    param temperature: pointer to float to store temperature
-    param humidity: pointer to float to store humidity
-    */
-    // function_tag
-    char* log_tag = "DHT11";
-
-    // Read sensor data
-    int ret = dht_read_float_data(SENSOR_TYPE, TEMP_SENSOR_PIN, humidity, temperature);
-    if (ret == ESP_OK) {
-        ESP_LOGI(log_tag, "Humidity: %.1f%% Temp: %.1fC", *humidity, *temperature);
-    } else {
-        ESP_LOGI(log_tag, "Could not read data from sensor");
-    }
-
-    return ret;
-}
-
-void read_moisture_sensor(int* moisture) {
-    /*
-    Reads moisture from a capacitive soil moisture sensor
-
-    param moisture: pointer to int to store moisture
-    */
-    // function_tag (Capactive Soil Moisture Sensor v1.2)
-    char* log_tag = "CSMSv1.2";
-
-    // Read sensor data
-    *moisture = adc1_get_raw(MOISTURE_SENSOR_ADC);
-    ESP_LOGI(log_tag, "Moisture Sensor raw value: %d", *moisture);
-}
-
-
-// Display task
-#define tag "SSD1306"
-
-void display_task() {
-    /*
-    /This task displays sensor data to the OLED display
-    */
-    SSD1306_t dev;
-	int center, top, bottom;
-	char lineChar[20];
-
-    #if CONFIG_I2C_INTERFACE
-        ESP_LOGI(tag, "INTERFACE is i2c");
-        ESP_LOGI(tag, "CONFIG_SDA_GPIO=%d",CONFIG_SDA_GPIO);
-        ESP_LOGI(tag, "CONFIG_SCL_GPIO=%d",CONFIG_SCL_GPIO);
-        ESP_LOGI(tag, "CONFIG_RESET_GPIO=%d",CONFIG_RESET_GPIO);
-	    i2c_master_init(&dev, CONFIG_SDA_GPIO, CONFIG_SCL_GPIO, CONFIG_RESET_GPIO);
-    #endif // CONFIG_I2C_INTERFACE
-    #if CONFIG_SPI_INTERFACE
-        ESP_LOGI(tag, "INTERFACE is SPI");
-        ESP_LOGI(tag, "CONFIG_MOSI_GPIO=%d",CONFIG_MOSI_GPIO);
-        ESP_LOGI(tag, "CONFIG_SCLK_GPIO=%d",CONFIG_SCLK_GPIO);
-        ESP_LOGI(tag, "CONFIG_CS_GPIO=%d",CONFIG_CS_GPIO);
-        ESP_LOGI(tag, "CONFIG_DC_GPIO=%d",CONFIG_DC_GPIO);
-        ESP_LOGI(tag, "CONFIG_RESET_GPIO=%d",CONFIG_RESET_GPIO);
-        spi_master_init(&dev, CONFIG_MOSI_GPIO, CONFIG_SCLK_GPIO, CONFIG_CS_GPIO, CONFIG_DC_GPIO, CONFIG_RESET_GPIO);
-    #endif // CONFIG_SPI_INTERFACE
-
-    #if CONFIG_SSD1306_128x64
-	ESP_LOGI(tag, "Panel is 128x64");
-	ssd1306_init(&dev, 128, 64);
-    #endif // CONFIG_SSD1306_128x64
-    #if CONFIG_SSD1306_128x32
-        ESP_LOGI(tag, "Panel is 128x32");
-        ssd1306_init(&dev, 128, 32);
-    #endif // CONFIG_SSD1306_128x32
-
-    #if CONFIG_SSD1306_128x64
-        top = 2;
-        center = 3;
-        bottom = 8;
-        ssd1306_display_text(&dev, 0, "SSD1306 128x64", 14, false);
-        ssd1306_display_text(&dev, 1, "ABCDEFGHIJKLMNOP", 16, false);
-        ssd1306_display_text(&dev, 2, "abcdefghijklmnop",16, false);
-        ssd1306_display_text(&dev, 3, "Hello World!!", 13, false);
-        //ssd1306_clear_line(&dev, 4, true);
-        //ssd1306_clear_line(&dev, 5, true);
-        //ssd1306_clear_line(&dev, 6, true);
-        //ssd1306_clear_line(&dev, 7, true);
-        ssd1306_display_text(&dev, 4, "SSD1306 128x64", 14, true);
-        ssd1306_display_text(&dev, 5, "ABCDEFGHIJKLMNOP", 16, true);
-        ssd1306_display_text(&dev, 6, "abcdefghijklmnop",16, true);
-        ssd1306_display_text(&dev, 7, "Hello World!!", 13, true);
-    #endif // CONFIG_SSD1306_128x64
-    vTaskDelay(3000 / portTICK_PERIOD_MS);
-	
-	// Display Count Down
-	uint8_t image[24];
-	memset(image, 0, sizeof(image));
-	ssd1306_display_image(&dev, top, (6*8-1), image, sizeof(image));
-	ssd1306_display_image(&dev, top+1, (6*8-1), image, sizeof(image));
-	ssd1306_display_image(&dev, top+2, (6*8-1), image, sizeof(image));
-	for(int font=0x39;font>0x30;font--) {
-		memset(image, 0, sizeof(image));
-		ssd1306_display_image(&dev, top+1, (7*8-1), image, 8);
-		memcpy(image, font8x8_basic_tr[font], 8);
-		if (dev._flip) ssd1306_flip(image, 8);
-		ssd1306_display_image(&dev, top+1, (7*8-1), image, 8);
-		vTaskDelay(1000 / portTICK_PERIOD_MS);
-	}
-	
-	// Scroll Up
-	ssd1306_clear_screen(&dev, false);
-	ssd1306_contrast(&dev, 0xff);
-	ssd1306_display_text(&dev, 0, "---Scroll  UP---", 16, true);
-	//ssd1306_software_scroll(&dev, 7, 1);
-	ssd1306_software_scroll(&dev, (dev._pages - 1), 1);
-	for (int line=0;line<bottom+10;line++) {
-		lineChar[0] = 0x01;
-		sprintf(&lineChar[1], " Line %02d", line);
-		ssd1306_scroll_text(&dev, lineChar, strlen(lineChar), false);
-		vTaskDelay(500 / portTICK_PERIOD_MS);
-	}
-	vTaskDelay(3000 / portTICK_PERIOD_MS);
-	
-	// Scroll Down
-	ssd1306_clear_screen(&dev, false);
-	ssd1306_contrast(&dev, 0xff);
-	ssd1306_display_text(&dev, 0, "--Scroll  DOWN--", 16, true);
-	//ssd1306_software_scroll(&dev, 1, 7);
-	ssd1306_software_scroll(&dev, 1, (dev._pages - 1) );
-	for (int line=0;line<bottom+10;line++) {
-		lineChar[0] = 0x02;
-		sprintf(&lineChar[1], " Line %02d", line);
-		ssd1306_scroll_text(&dev, lineChar, strlen(lineChar), false);
-		vTaskDelay(500 / portTICK_PERIOD_MS);
-	}
-	vTaskDelay(3000 / portTICK_PERIOD_MS);
-
-	// Page Down
-	ssd1306_clear_screen(&dev, false);
-	ssd1306_contrast(&dev, 0xff);
-	ssd1306_display_text(&dev, 0, "---Page	DOWN---", 16, true);
-	ssd1306_software_scroll(&dev, 1, (dev._pages-1) );
-	for (int line=0;line<bottom+10;line++) {
-		//if ( (line % 7) == 0) ssd1306_scroll_clear(&dev);
-		if ( (line % (dev._pages-1)) == 0) ssd1306_scroll_clear(&dev);
-		lineChar[0] = 0x02;
-		sprintf(&lineChar[1], " Line %02d", line);
-		ssd1306_scroll_text(&dev, lineChar, strlen(lineChar), false);
-		vTaskDelay(500 / portTICK_PERIOD_MS);
-	}
-	vTaskDelay(3000 / portTICK_PERIOD_MS);
-
-	// Horizontal Scroll
-	ssd1306_clear_screen(&dev, false);
-	ssd1306_contrast(&dev, 0xff);
-	ssd1306_display_text(&dev, center, "Horizontal", 10, false);
-	ssd1306_hardware_scroll(&dev, SCROLL_RIGHT);
-	vTaskDelay(5000 / portTICK_PERIOD_MS);
-	ssd1306_hardware_scroll(&dev, SCROLL_LEFT);
-	vTaskDelay(5000 / portTICK_PERIOD_MS);
-	ssd1306_hardware_scroll(&dev, SCROLL_STOP);
-	
-	// Vertical Scroll
-	ssd1306_clear_screen(&dev, false);
-	ssd1306_contrast(&dev, 0xff);
-	ssd1306_display_text(&dev, center, "Vertical", 8, false);
-	ssd1306_hardware_scroll(&dev, SCROLL_DOWN);
-	vTaskDelay(5000 / portTICK_PERIOD_MS);
-	ssd1306_hardware_scroll(&dev, SCROLL_UP);
-	vTaskDelay(5000 / portTICK_PERIOD_MS);
-	ssd1306_hardware_scroll(&dev, SCROLL_STOP);
-	
-	// Invert
-	ssd1306_clear_screen(&dev, true);
-	ssd1306_contrast(&dev, 0xff);
-	ssd1306_display_text(&dev, center, "  Good Bye!!", 12, true);
-	vTaskDelay(5000 / portTICK_PERIOD_MS);
-
-
-	// Fade Out
-	ssd1306_fadeout(&dev);
-	
-    #if 0
-        // Fade Out
-        for(int contrast=0xff;contrast>0;contrast=contrast-0x20) {
-            ssd1306_contrast(&dev, contrast);
-            vTaskDelay(40);
-        }
-    #endif
-
-	// Restart module
-	esp_restart();
 }
 
 void app_main()
@@ -347,9 +150,6 @@ void app_main()
     // Initialize sensor queue
     sensor_queue = xQueueCreate(10, sizeof(sensor_data_t));
 
-    // xTaskCreate(dht_test, "dht_test", configMINIMAL_STACK_SIZE * 3, NULL, 5, NULL);
-    // xTaskCreate(moisture_sensor_test, "moisture_sensor_test", configMINIMAL_STACK_SIZE * 3, NULL, 5, NULL);
-    // xTaskCreate(blink_test, "blink_test", configMINIMAL_STACK_SIZE * 3, NULL, 5, NULL);
     xTaskCreate(sensor_task, "sensor_task", configMINIMAL_STACK_SIZE * 3, NULL, 5, NULL);
     xTaskCreate(display_sensor_data_task, "display_sensor_data_task", configMINIMAL_STACK_SIZE * 3, NULL, 5, NULL);
 }
